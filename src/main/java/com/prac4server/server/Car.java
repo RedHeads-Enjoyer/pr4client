79e248310766cf0673319e215cd1ccf06b30b6cc
RedHeads-Enjoyer <dsc.cxc@mail.ru>
2024-09-18 18:30:20 +0300

    Initial commit

---
+++ src/main/java/com/prac4server/server/Car.java
@@ -0,0 +1,8 @@
+package com.prac4server.server;
+
+public class Car {
+    private Integer id;
+    private String model;
+    private String color;
+    private Float price;
+}
---
+++ src/main/java/com/prac4server/server/RequestStreamController.java
@@ -0,0 +1,27 @@
+package com.prac4server.server;
+
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cats")
+public class RequestStreamController {
+    private final RSocketRequester rSocketRequester;
+
+    @Autowired
+    public RequestStreamController(RSocketRequester rSocketRequester) {
+        this.rSocketRequester = rSocketRequester;
+    }
+
+    @GetMapping
+    public Publisher<Car> getCars() {
+        return rSocketRequester
+                .route("getCars")
+                .data(new Car())
+                .retrieveFlux(Car.class);
+    }
+}
